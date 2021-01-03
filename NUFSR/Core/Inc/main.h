2817fcba4c0cea8050e1321041f4082ac0bd9199
Benjamin James Young <BenjaminYoung7198@gmail.com>
2021-01-03 13:31:24 +1100

    Added STM32CubeMX Workspace files.
    Got UART transmission/receive coms working using Interrupts. In addition, the NUFRSR.ioc file has been updated to correctly match the NUfsr board schematics.

---
+++ NUFSR/Core/Inc/main.h
@@ -0,0 +1,117 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f3xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+typedef enum
+{
+  LED_ON = 0U,
+  LED_OFF = !LED_ON
+} LEDStatus;
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define XTAL_32MHZ_IN_Pin GPIO_PIN_0
+#define XTAL_32MHZ_IN_GPIO_Port GPIOF
+#define XTAL_32MHZ_OUT_Pin GPIO_PIN_1
+#define XTAL_32MHZ_OUT_GPIO_Port GPIOF
+#define FSR1_Pin GPIO_PIN_0
+#define FSR1_GPIO_Port GPIOA
+#define FSR2_Pin GPIO_PIN_1
+#define FSR2_GPIO_Port GPIOA
+#define FSR3_Pin GPIO_PIN_2
+#define FSR3_GPIO_Port GPIOA
+#define FSR4_Pin GPIO_PIN_3
+#define FSR4_GPIO_Port GPIOA
+#define MPU_INT_Pin GPIO_PIN_8
+#define MPU_INT_GPIO_Port GPIOD
+#define MPU_NSS_Pin GPIO_PIN_6
+#define MPU_NSS_GPIO_Port GPIOC
+#define MPU_CLK_Pin GPIO_PIN_7
+#define MPU_CLK_GPIO_Port GPIOC
+#define MPU_MISO_Pin GPIO_PIN_8
+#define MPU_MISO_GPIO_Port GPIOC
+#define MPU_MOSI_Pin GPIO_PIN_9
+#define MPU_MOSI_GPIO_Port GPIOC
+#define DXL_DIR_Pin GPIO_PIN_8
+#define DXL_DIR_GPIO_Port GPIOA
+#define DXL_P_Pin GPIO_PIN_9
+#define DXL_P_GPIO_Port GPIOA
+#define DXL_N_Pin GPIO_PIN_10
+#define DXL_N_GPIO_Port GPIOA
+#define JTAG_TMS_Pin GPIO_PIN_13
+#define JTAG_TMS_GPIO_Port GPIOA
+#define JTAG_TCK_Pin GPIO_PIN_14
+#define JTAG_TCK_GPIO_Port GPIOA
+#define JTAG_TDI_Pin GPIO_PIN_15
+#define JTAG_TDI_GPIO_Port GPIOA
+#define JTAG_TDO_Pin GPIO_PIN_3
+#define JTAG_TDO_GPIO_Port GPIOB
+#define JTAG_RST_Pin GPIO_PIN_4
+#define JTAG_RST_GPIO_Port GPIOB
+#define LED1_Pin GPIO_PIN_8
+#define LED1_GPIO_Port GPIOB
+#define LED2_Pin GPIO_PIN_9
+#define LED2_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ NUFSR/Core/Src/usart.c
@@ -0,0 +1,241 @@
+/**
+  ******************************************************************************
+  * @file    usart.c
+  * @brief   This file provides code for the configuration
+  *          of the USART instances.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+// TODO: Develop a better error checking and management system
+// TODO: Write code for Interrupt Error ISR
+// TODO: Write function table for UART utilities
+/* Includes ------------------------------------------------------------------*/
+#include "usart.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+UART_HandleTypeDef huart1;
+NUfsr_UART_IT_StateHandler huart1_ITh;
+
+/* USART1 init function */
+
+void MX_USART1_UART_Init(void)
+{
+
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 38400;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+}
+
+void HAL_UART_MspInit(UART_HandleTypeDef* uartHandle)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if(uartHandle->Instance==USART1)
+  {
+  /* USER CODE BEGIN USART1_MspInit 0 */
+  huart1_ITh.Tx_State = Tx_FINISHED;
+  huart1_ITh.Rx_State = Rx_FINISHED;
+  /* USER CODE END USART1_MspInit 0 */
+  /* USART1 clock enable */
+  	__HAL_RCC_USART1_CLK_ENABLE();
+
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+	/**USART1 GPIO Configuration
+	PA9     ------> USART1_TX
+	PA10     ------> USART1_RX
+	*/
+	GPIO_InitStruct.Pin = DXL_P_Pin|DXL_N_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+	GPIO_InitStruct.Alternate = GPIO_AF7_USART1;
+	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN USART1_MspInit 1 */
+
+  /* USER CODE END USART1_MspInit 1 */
+  }
+}
+
+void HAL_UART_MspDeInit(UART_HandleTypeDef* uartHandle)
+{
+
+  if(uartHandle->Instance==USART1)
+  {
+  /* USER CODE BEGIN USART1_MspDeInit 0 */
+
+  /* USER CODE END USART1_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_USART1_CLK_DISABLE();
+
+    /**USART1 GPIO Configuration
+    PA9     ------> USART1_TX
+    PA10     ------> USART1_RX
+    */
+    HAL_GPIO_DeInit(GPIOA, DXL_P_Pin|DXL_N_Pin);
+
+    /* USART1 interrupt Deinit */
+    HAL_NVIC_DisableIRQ(USART1_IRQn);
+  /* USER CODE BEGIN USART1_MspDeInit 1 */
+
+  /* USER CODE END USART1_MspDeInit 1 */
+  }
+}
+
+/* USER CODE BEGIN 1 */
+
+/*
+ * ISR
+ */
+void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart)
+{
+	/* Tx Complete Signal */
+	huart1_ITh.Tx_State = Tx_FINISHED;
+}
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+	/* Rx Complete Signal */
+	huart1_ITh.Rx_State = Rx_FINISHED;
+}
+
+/*
+ * Tx and Rx operations
+ */
+HAL_StatusTypeDef NUfsr_UART_Transmit(UART_HandleTypeDef *huart, void *pData, uint16_t Byte_Size)
+{
+	// Error checking
+	if((huart == NULL) || (pData == NULL) || (Byte_Size == 0U))
+		return HAL_ERROR;
+
+	// Wait for Rx poll
+	NUfsr_UART_Poll_Rx(huart, HAL_MAX_DELAY);
+
+	// Update Tx state
+	huart1_ITh.Tx_State = Tx_NOT_FINISHED;
+
+	// Set DXL_DIR
+	HAL_GPIO_WritePin(GPIOA, DXL_DIR_Pin, Tx);
+
+	// Begin transmission
+	return HAL_UART_Transmit_IT(huart, (uint8_t*)pData, Byte_Size);
+
+}
+
+HAL_StatusTypeDef NUfsr_UART_Receive(UART_HandleTypeDef *huart, void *pData, uint16_t Byte_Size)
+{
+	// Error checking
+	if((huart == NULL) || (pData == NULL) || (Byte_Size == 0U))
+		return HAL_ERROR;
+
+	// Wait for Rx poll
+	NUfsr_UART_Poll_Tx(huart, HAL_MAX_DELAY);
+
+	// Update Rx state
+	huart1_ITh.Rx_State = Tx_NOT_FINISHED;
+
+	// Set DXL_DIR
+	HAL_GPIO_WritePin(GPIOA, DXL_DIR_Pin, Rx);
+
+	// Begin transmission
+	return HAL_UART_Receive_IT(huart, (uint8_t*)pData, Byte_Size);
+}
+/*
+ * Polling
+ */
+HAL_StatusTypeDef NUfsr_UART_Poll_Tx(UART_HandleTypeDef *huart, uint32_t Timeout)
+{
+	// Error checking
+	if((huart == NULL) || (Timeout == 0U))
+		return HAL_ERROR;
+
+	uint32_t tickstart = HAL_GetTick();
+
+	// Begin Polling
+	while(!NUfsr_UART_Tx_StatusComplete(huart))
+	{
+		if(Timeout != HAL_MAX_DELAY)
+		{
+			if(HAL_GetTick() - tickstart > Timeout)
+			{
+				// Handle Error
+				return HAL_ERROR;
+			}
+		}
+	}
+
+	return HAL_OK;
+}
+
+HAL_StatusTypeDef NUfsr_UART_Poll_Rx(UART_HandleTypeDef *huart, uint32_t Timeout)
+{
+	// Error checking
+	if(huart == NULL)
+		return HAL_ERROR;
+
+	uint32_t tickstart = HAL_GetTick();
+
+	// Begin Polling
+	while(!NUfsr_UART_Rx_StatusComplete(huart))
+	{
+		if(Timeout != HAL_MAX_DELAY)
+		{
+			if(HAL_GetTick() - tickstart > Timeout)
+			{
+				// Handle Error
+				return HAL_ERROR;
+			}
+		}
+	}
+
+	return HAL_OK;
+}
+
+/*
+ * Manual Polling
+ */
+bool NUfsr_UART_Tx_StatusComplete(UART_HandleTypeDef *huart)
+{
+	if(huart1_ITh.Tx_State == Tx_FINISHED)
+		return true;
+
+	return false;
+}
+
+bool NUfsr_UART_Rx_StatusComplete(UART_HandleTypeDef *huart)
+{
+
+	if(huart1_ITh.Rx_State == Rx_FINISHED)
+		return true;
+
+	return false;
+}
+
+/* USER CODE END 1 */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
