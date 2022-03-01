cb33dbb07bfdf0fc1f9f9aebe963e4a784059478
Benjamin James Young <BenjaminYoung7198@gmail.com>
2022-03-01 14:33:34 +1100

    Fix IOC Config
    Fixed:
    SPI Boardrate presale from 16-->256
    Changed DXL_N_Pin from PP to OD

--- NUFSR/Core/Src/usart.c
+++ NUFSR/Core/Src/usart.c
@@ -76,12 +76,19 @@ void HAL_UART_MspInit(UART_HandleTypeDef* uartHandle)
     PA9     ------> USART1_TX
     PA10     ------> USART1_RX
     */
-    GPIO_InitStruct.Pin = DXL_P_Pin|DXL_N_Pin;
+    GPIO_InitStruct.Pin = DXL_P_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
     GPIO_InitStruct.Alternate = GPIO_AF7_USART1;
-    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+    HAL_GPIO_Init(DXL_P_GPIO_Port, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = DXL_N_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF7_USART1;
+    HAL_GPIO_Init(DXL_N_GPIO_Port, &GPIO_InitStruct);
 
   /* USER CODE BEGIN USART1_MspInit 1 */
 
